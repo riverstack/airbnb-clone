490e424e75f8d1d90a846966cb8b0ca9ce09cda4
riverstack <taewonkim168@gmail.com>
2019-11-08 13:03:40 +0900

    #9 Seeding

---
+++ reviews/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.Review)
+class RevieAdmin(admin.ModelAdmin):
+
+    """ Review Admin Definition """
+
+    list_display = ("__str__", "rating_average")
+
+    pass
---
+++ users/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "langauge",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
+    list_filter = UserAdmin.list_filter + ("superhost",)
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "langauge",
+        "currency",
+        "superhost",
+        "is_staff",
+        "is_superuser",
+    )
